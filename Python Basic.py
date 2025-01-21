8ae2ef74a09c182da45f57b565f328c7d72f215f
Biplob Chakma <63535926+biplob-c@users.noreply.github.com>
2025-01-21 18:27:59 +0600

    Create Python Basic.py

---
+++ Python Basic.py
@@ -0,0 +1,11 @@
+a = input("Enter some text: ")
+
+b = a.split(" ")
+print("Split Text is: ", b)
+
+w_count = len(b)
+print("Total number of words: ", w_count)
+
+length = len(a)
+print("Total number of characters: ", length)
+/////////////////////////////
