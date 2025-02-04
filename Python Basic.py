6e75f7cf857ecc2796d4a2e6d3095ad7b9fd6581
Biplob Chakma <63535926+biplob-c@users.noreply.github.com>
2025-02-04 18:15:46 +0600

    Update Python Basic.py

--- Python Basic.py
+++ Python Basic.py
@@ -9,3 +9,66 @@ print("Total number of words: ", w_count)
 length = len(a)
 print("Total number of characters: ", length)
 /////////////////////////////
+
+
+"""#01
+a = "exercises"
+x = (a[:2])
+y = (a[7:9])
+z = x+y
+print(z)
+
+b = len(a);
+print("length of 'a': ", b)
+'''if b < 2:
+    return null '''
+
+#10
+c = "SoNargaon UniverSity";
+print(c.lower())
+
+#07
+re = "Dhaka"
+print("Input:", re)
+print("Reversed:", re)
+
+#09
+bs = input("Enter a string: ")
+def binary_string(s):
+    return all (char in '01' for char in s) and len(s) > 0
+if binary_string(bs):
+    print("yes")
+
+"""
+
+a = input("Enter some text: ")
+
+b = a.split(" ")
+print("Split Text is: ", b)
+
+w_count = len(b)
+print("Total number of words: ", w_count)
+
+length = len(a)
+print("Total number of characters: ", length)
+print("///////////////////////////////////////////")
+
+print("Split a string into a list where each word is a list item:")
+txt = "Dhaka is the capital of Bangladesh."
+x = txt.split()
+print("Words are (as a list): ", x)
+y = len(x)
+print("Total number of words: ", y)
+
+z = len(txt)
+print("Total number of characters: ", z)
+print("///////////////////////////////////////////")
+
+
+
+
+
+
+
+
+
